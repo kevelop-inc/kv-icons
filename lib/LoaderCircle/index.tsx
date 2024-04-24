6a6fa2ba92db7815bc3340a48157f8c2815879eb
ZeroGDrive <ayoubyf@gmail.com>
2024-04-24 20:47:35 +0200

    🐛 Fix react specific props for svg

--- lib/LoaderCircle/index.tsx
+++ lib/LoaderCircle/index.tsx
@@ -16,9 +16,9 @@ export const LoaderCircle = forwardRef<SVGSVGElement, Props>(
         viewBox="0 0 24 24"
         fill="none"
         stroke={color}
-        stroke-width={strokeWidth}
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
         {...props}
       >
         <path d="M21 12a9 9 0 1 1-6.219-8.56" />
